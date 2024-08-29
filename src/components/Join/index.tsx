958bba3a910db51a5bd0a19a891434a6219a9b6b
Anne Brown <anne@browndomain.com>
2024-08-29 15:42:39 -0400

    preview

--- src/components/Join/index.tsx
+++ src/components/Join/index.tsx
@@ -6,17 +6,17 @@ const Join = () => {
   return (
     <>
       <section id="Join" className="z-30">
-        <div className="relative float-left w-1/2 pl-10 items-center justify-cente">
-          <p className="">
+        <div className="relative float-left w-1/2 pl-10 items-center justify-center">
+          <p className="pb-0 mb-0 h-full text-justify">
             Join us as we gather together our collective love of this ecological and historical gem to counter the development proposal. Together we can find a better and more sustainable solution that meets the needs of all, including; the Galilee Center, a respected retreat center that has been entrusted with the care of this land for many years; the town’s folk who live nearby or use this area for their physical and spiritual wellbeing; the flora, fauna and fungi of the great and incredibly rare forest known as Gillies Grove; and the developer who needs to find ways to build homes for a growing Ontario population.
             <br /><br />
             Together we can find a better way!
           </p>
         </div>
-        <div className="relative float-right w-1/2 p-0 m-0">
+        <div className="relative float-right w-2/5 p-0 pb-10 mb-5 m-0">
           <Image
             className=""
-            src="/images/vignette.jpg"
+            src="/images/vignette.svg"
             alt="Field"
             loading="eager"
             priority
