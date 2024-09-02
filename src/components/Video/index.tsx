937b3db0173b7cd7e7aaefe7e51b681b28818028
Anne Brown <anne@browndomain.com>
2024-09-02 13:20:04 -0400

    video size

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -10,10 +10,23 @@ const Video = () => {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <section className="z-10 w-full h-auto rounded-lg">
-        <div className="container mx-auto  w-full 1xs:w-screen sm:w-screen md:w-screen h-auto">
-            <iframe width="80vh" height="auto" src="https://www.youtube.com/embed/tEgtT1LX-Sk" title="Save the Grove Again"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+    <section className="z-10 max-w-80% p-0 pl-20 m-0 mx-auto">
+        <div className="container m-0 p-0 object-center">
+           
         </div>
+
+        <div className="-mx-4 flex flex-wrap">
+          <div className="w-full px-4">
+            <div
+              className="mx-auto max-w-[770px] overflow-hidden rounded-md"
+              data-wow-delay=".15s"
+            >
+              <iframe width="897" height="506" src="https://www.youtube.com/embed/tEgtT1LX-Sk" title="Save the Grove Again"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+            </div>
+          </div>
+        </div>
+
+
     </section>
   );
 };
