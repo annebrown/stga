d5cbfac2c0fe2215a0444a1c3ea3c7c8ddf90c96
Anne Brown <anne@browndomain.com>
2024-09-02 13:29:01 -0400

    Blog Coming Soon!

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -4,7 +4,7 @@ const Blog = () => {
       id="blog"
       className="bg-gray-light py-16 md:py-20 lg:py-28"
     >
-      <h1><Blow></Blow></h1>
+      <h1>Blog</h1>
       <p>Coming Soon!</p>
     </section>
   );
