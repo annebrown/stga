fb7a20dfebe81e8350cf4efcec201f0cc8bd3c97
Anne Brown <anne@browndomain.com>
2024-09-02 08:50:38 -0400

    video

--- src/components/Join/index.tsx
+++ src/components/Join/index.tsx
@@ -6,9 +6,9 @@ const Join = () => {
   return (
     <>
       <section id="Join" className="clear-both">
-        <div className=" w-full z-30 ml-1.5 mr-3 pt-5 grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 justify-center">
-            <div className="pl-5">
-                <p className="pt-5
+        <div className=" w-full z-30 ml-1.5 mr-3 mb-5 pt-5 px-auto py-auto grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 align-items-center">
+            <div className="pl-5 py-auto my-auto">
+                <p className="pt-5 m-5
                 text-black
                 text-2xs
                 xs:text-1xs
@@ -17,9 +17,9 @@ const Join = () => {
                 lg:text-base
                 1xl:text-lg
                 ">
-                Join us as we gather together our collective love of this ecological and historical gem to counter the development proposal. Together we can find a better and more sustainable solution that meets the needs of all, including; the Galilee Center, a respected retreat center that has been entrusted with the care of this land for many years; the town’s folk who live nearby or use this area for their physical and spiritual wellbeing; the flora, fauna and fungi of the great and incredibly rare forest known as Gillies Grove; and the developer who needs to find ways to build homes for a growing Ontario population.
-                </p><br />
-                <p className="
+                    Join us as we gather together our collective love of this ecological and historical gem to counter the development proposal. Together we can find a better and more sustainable solution that meets the needs of all, including; the Galilee Center, a respected retreat center that has been entrusted with the care of this land for many years; the town’s folk who live nearby or use this area for their physical and spiritual wellbeing; the flora, fauna and fungi of the great and incredibly rare forest known as Gillies Grove; and the developer who needs to find ways to build homes for a growing Ontario population.
+                </p>
+                <p className=" m-5
                 bold
                 text-[#165D8F]
                 text-sm
@@ -35,7 +35,6 @@ const Join = () => {
             </div>
           <div className="m-5">
             <Image
-              className=""
               src="/images/vignette.svg"
               alt="Field"
               loading="eager"
--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -10,9 +10,9 @@ const Video = () => {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <section className="z-10 w-full p-0 m-0">
-        <div className="p-0 m-0 container mx-auto rounded-lg  w-full align-items-center">
-            <iframe width="1193" height="671" src="https://www.youtube.com/embed/tEgtT1LX-Sk" title="Save the Grove Again"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen />
+    <section className="z-10 w-full">
+        <div className="container mx-auto rounded-lg">
+            <iframe width="1193" height="671" src="https://www.youtube.com/embed/tEgtT1LX-Sk" title="Save the Grove Again"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
         </div>
     </section>
   );
