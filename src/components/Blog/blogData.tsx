27bed3a6bf89a42edb599de274c8bc7fe1819542
Anne Brown <anne@browndomain.com>
2024-09-05 19:56:22 -0400

    l8test for review

--- src/components/Blog/blogData.tsx
+++ src/components/Blog/blogData.tsx
@@ -6,42 +6,15 @@ const blogData: Blog[] = [
     title: "Save the Grove Again Information Session- Planting the Seed",
     paragraph:
       "Join us for an information session for the Save the Grove Again campaign.",
-    image: "/images/blog/lawn-sign.jpg",
+    image: "/images/blog/P1040438.JPG",
     author: {
-      name: "Lacey ??",
-      image: "/images/blog/author-01.png",
-      designation: "Title?",
+      name: "",
+      image: "",
+      designation: "",
     },
     tags: ["event"],
-    publishDate: "2024-09-05",
+    publishDate: "September 05, 2024",
   },
-  {
-    id: 2,
-    title: "9 simple ways to improve your design skills",
-    paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/blog/blog-02.jpg",
-    author: {
-      name: "Musharof Chy",
-      image: "/images/blog/author-02.png",
-      designation: "Content Writer",
-    },
-    tags: ["computer"],
-    publishDate: "2025",
-  },
-  {
-    id: 3,
-    title: "Tips to quickly improve your coding speed.",
-    paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/blog/blog-03.jpg",
-    author: {
-      name: "Lethium Deo",
-      image: "/images/blog/author-03.png",
-      designation: "Graphic Designer",
-    },
-    tags: ["design"],
-    publishDate: "2025",
-  },
+
 ];
 export default blogData;
