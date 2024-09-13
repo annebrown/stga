f6df33c4ae1ce8f9b42c2dd0b261d640f68cba2d
Anne Brown <anne@browndomain.com>
2024-09-13 13:21:46 -0400

    town list

--- src/components/Action/index.tsx
+++ src/components/Action/index.tsx
@@ -11,23 +11,29 @@ const Action = () => {
         <div className="pb-10 grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 md:gap-x-6 lg:gap-x-8 xl:grid-cols-3 h-full">
           
             <div className="w-full bg-gray-50 p-5">
-                <h2 className="text-[#165D8F] text-lg font-extrabold">SPREAD THE WORD</h2>
+                <h2 className="text-[#165D8F] text-lg font-extrabold">
+                    SPREAD THE WORD
+                </h2>
                 <p className="text-left py-2">
-                We have had a lot of media attention. Let&apos;s keep the ball rolling! Keep telling others, wear your button, hand out flyers, and follow, like and share our Facebook page. To request buttons, lawn signs and flyers, send an email to: <span className="text-[#165D8F] font-extrabold">savethegroveagain@gmail.com</span>.
+                    We have had a lot of media attention. Let&apos;s keep the ball rolling! Keep telling others, wear your button, hand out flyers, and follow, like and share our Facebook page. To request buttons, lawn signs and flyers, send an email to: <span className="text-[#165D8F] font-extrabold">savethegroveagain@gmail.com</span>.
                 </p>
             </div>
 
             <div className="w-full bg-gray-50 p-5">
-                <h2 className="text-[#165D8F] text-lg font-extrabold">STAY INFORMED</h2>
+                <h2 className="text-[#165D8F] text-lg font-extrabold">
+                    STAY INFORMED
+                </h2>
                 <p className="text-left py-2">
                     To receive the latest updates, announcements, events, and media coverage, subscribe to our <a className="text-[#38AF34] font-extrabold" href="https://docs.google.com/forms/d/e/1FAIpQLSfBFJHDmUnmpGv1XaSHGaSSPmGizbxrQ-KgeqRRZqdVNZeuHg/viewform">mailing list</a>.
                 </p>
             </div>
 
             <div className="w-full bg-gray-50 p-5">
-                <h2 className="text-[#165D8F] text-lg font-extrabold">SIGN THE PETITION</h2>
+                <h2 className="text-[#165D8F] text-lg font-extrabold">
+                    SIGN THE PETITION
+                </h2>
                 <p className="text-left py-2">
-                Take a moment to sign the <a className="text-[#38AF34] font-extrabold"  href="https://www.change.org/p/save-the-grove-again-7292e6ca-eb8e-426a-8809-0bb608f24120">online petition</a> 
+                    Take a moment to sign the <a className="text-[#38AF34] font-extrabold"  href="https://www.change.org/p/save-the-grove-again-7292e6ca-eb8e-426a-8809-0bb608f24120">online petition</a> 
                 </p>
                 <div className="p-2 bg-[#fbbf24] text-black text-xs m-1">
                     WARNING: <br/><br/>Donations to change.org DO NOT go towards our cause.  For donations to STGA, please use Interac E-Transfer to: <span className="text-[#165D8F] font-extrabold">savethegroveagain@gmail.com</span>.
@@ -36,122 +42,159 @@ const Action = () => {
             </div>
 
             <div className="w-full bg-gray-50 p-5">
-                <h2 className="text-[#165D8F] text-lg font-extrabold">VOLUNTEER</h2>
+                <h2 className="text-[#165D8F] text-lg font-extrabold">
+                    VOLUNTEER
+                </h2>
                 <p className="text-left py-2">
-                We have set up a volunteer form. If you would like to offer your help please take a few minutes to fill in the <a  className="text-[#38AF34] font-extrabold" href="https://docs.google.com/forms/d/e/1FAIpQLSc611weSVb3qvnl6KJd6AZ_lmRn0yStEYDpxIkE9bVJhFTeUQ/viewform?usp=sf_link">form</a>.
+                    We have set up a volunteer form. If you would like to offer your help please take a few minutes to fill in the <a  className="text-[#38AF34] font-extrabold" href="https://docs.google.com/forms/d/e/1FAIpQLSc611weSVb3qvnl6KJd6AZ_lmRn0yStEYDpxIkE9bVJhFTeUQ/viewform?usp=sf_link">form</a>.
                 </p>
             </div>
 
             <div className="w-full bg-gray-50 p-5">
-                <h2 className="text-[#165D8F] text-lg font-extrabold">MAKE A DONATION</h2>
+                <h2 className="text-[#165D8F] text-lg font-extrabold">
+                    MAKE A DONATION
+                </h2>
                 <p>
-                Donations are used to cover the cost of materials including lawn signs, flyers, posters, buttons, website domain name etc. For those wanting to make a donation, please direct your Interac E-Transfer to: <span className="text-[#165D8F] font-extrabold">savethegroveagain@gmail.com</span>. Please share that information with others who may want to contribute.
-                               
+                    Donations are used to cover the cost of materials including lawn signs, flyers, posters, buttons, website domain name etc. For those wanting to make a donation, please direct your Interac E-Transfer to: <span className="text-[#165D8F] font-extrabold">savethegroveagain@gmail.com</span>. Please share that information with others who may want to contribute.             
                 </p>
             </div>
 
             <div className="w-full bg-gray-50 p-5">
-                <h2 className="text-[#165D8F] text-lg font-extrabold">SPEAK OUT!</h2>
+                <h2 className="text-[#165D8F] text-lg font-extrabold">
+                    SPEAK OUT!
+                </h2>
                 <p className="text-left py-2">
-                    Let’s not underestimate the power of community. 
+                    Let&quot;s not underestimate the power of community. 
                 </p>
                 <p className="text-left py-2">
-                Below you will find contact information for the parties involved in the sale of this land, as well as contacts for all levels of government. We urge anyone who opposes the development of adjacent land to Gillies Grove to call or email these parties and get your voice heard! 
+                    Below you will find contact information for the parties involved in the sale of this land, as well as contacts for all levels of government. We urge anyone who opposes the development of adjacent land to Gillies Grove to call or email these parties and get your voice heard! 
                 </p>
             </div>
         </div>
-<div className="p-10 bg-green-200">
-        <h1 className="text-3xl font-bold text-center pb-10 text-black center">
-        List of Contacts
-        </h1>
-        <div className="rounded-lg grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 md:gap-x-6 lg:gap-x-8 xl:grid-cols-3 h-full ">
+        <div className="p-10 bg-green-200">
+            <h1 className="text-3xl font-bold text-center pb-10 text-black center">
+                List of Contacts
+             </h1>
+            <div className="rounded-lg grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 md:gap-x-6 lg:gap-x-8 xl:grid-cols-3 h-full ">
 
-    
+                <div className="rounded-lg w-full bg-white p-5">
+                    <p className="text-wrap">
+                        <a href="https://www.omilacombe.ca" className="text-[#165D8F]">
+                            <strong>Missionary Oblates of Mary Immaculate Lacombe Canada</strong>
+                        </a><br/>
+                        <a href="https://www.omilacombe.ca" className="text-green-500">
+                            https://www.omilacombe.ca
+                        </a><br/>
+                        Attention Provincial Council members<br/>
+                        information@omilacombe.ca<br/>
+                        (613) 230-2225<br/>
+                    </p>
+                </div>
+                <div className="rounded-lg w-full bg-white p-5">
+                    <p className="text-wrap">
+                        <a href="https://www.galileecentre.com" className="text-[#165D8F]">
+                            <strong>Galilee Centre</strong>
+                        </a><br/>
+                        <a href="https://www.galileecentre.com" className="text-green-500">
+                            https://www.galileecentre.com
+                        </a><br/>
+                        Board President: David Perrin<br/>
+                        communications@galileecentre.com<br/>
+                    </p>
+                </div>
 
-            <div className="rounded-lg w-full bg-green-500 p-5">
-                <p className="text-wrap">
-                    <a href="https://www.omilacombe.ca" className="text-[#165D8F]"><strong>Missionary Oblates of Mary Immaculate Lacombe Canada</strong></a><br/>
-                    <a href="https://www.omilacombe.ca" className="text-white">https://www.omilacombe.ca</a><br/>
-                    Attention Provincial Council members<br/>
-                    information@omilacombe.ca<br/>
-                    (613) 230-2225<br/>
-                </p>
-            </div>
-            <div className="rounded-lg w-full bg-green-500 p-5">
-                <p className="text-wrap">
-                    <a href="https://www.galileecentre.com" className="text-[#165D8F]"><strong>Galilee Centre</strong></a><br/>
-                    <a href="https://www.galileecentre.com" className="text-white">https://www.galileecentre.com</a><br/>
-                    Board President: David Perrin<br/>
-                    communications@galileecentre.com<br/>
-                </p>
-            </div>
+                <div className="rounded-lg w-full bg-white p-5">
+                    <p className="text-wrap">
+                        <a href="https://www.thomascavanagh.ca" className="text-[#165D8F]">
+                            <strong>Thomas Cavanagh Construction Limited (Cavanagh Communities)</strong>
+                        </a><br/>
+                        <a href="https://www.thomascavanagh.ca" className="text-green-500">
+                            https://www.thomascavanagh.ca
+                        </a><br/>
+                        Owner: Jeff Cavanagh<br/>
+                        Contact: Pierre Dufresne<br/>
+                        pdufresne@thomascavanagh.ca<br/>
+                        (613) 257-2918<br/>
+                    </p>
+                </div>
 
+                <div className="rounded-lg w-full bg-white p-5">
+                    <p className="text-wrap">
+                        <a href="https://johnyakabuski.com" className="text-[#165D8F]">
+                            <strong>Legislative Assembly of Ontario</strong>
+                        </a><br/>
+                        <a href="https://johnyakabuski.com" className="text-green-500">
+                            https://johnyakabuski.com
+                        </a><br/>
+                        John Yakabuski<br/>
+                        John.Yakabuski@pc.ola.org<br/>
+                    </p>
+                </div>
 
-            <div className="rounded-lg w-full bg-green-500 p-5">
-                <p className="text-wrap">
-                    <a href="https://www.thomascavanagh.ca" className="text-[#165D8F]"><strong>Thomas Cavanagh Construction Limited (Cavanagh Communities)</strong></a><br/>
-                    <a href="https://www.thomascavanagh.ca" className="text-white">https://www.thomascavanagh.ca</a><br/>
-                    Owner: Jeff Cavanagh<br/>
-                    Contact: Pierre Dufresne<br/>
-                    pdufresne@thomascavanagh.ca<br/>
-                    Tel: (613) 257-2918<br/>
-                </p>
+                <div className="rounded-lg w-full bg-white p-5">
+                    <p className="text-wrap">
+                        <a href="http://cherylgallant.com" className="text-[#165D8F]">
+                            <strong>Member of Parliament</strong>
+                        </a><br/>
+                        <a href="http://cherylgallant.com" className="text-green-500">
+                            https://johnyakabuski.com
+                        </a><br/>
+                        Cheryl Gallant<br/>
+                        cheryl.gallant@parl.gc.ca<br/>
+                    </p>
+                </div>
             </div>
-            <div className="rounded-lg w-full bg-green-500 p-5">
-                <p className="text-wrap">
-                <a href="https://johnyakabuski.com" className="text-[#165D8F]"><strong>Legislative Assembly of Ontario</strong></a><br/>
-                   <a href="https://johnyakabuski.com" className="text-white">https://johnyakabuski.com</a><br/>
-                   John Yakabuski<br/>
-                   John.Yakabuski@pc.ola.org<br/>
-                </p>
-            </div>
+        </div>
 
-            <div className="rounded-lg w-full bg-green-500 p-5">
-                <p className="text-wrap">
-                <a href="http://cherylgallant.com" className="text-[#165D8F]"><strong>Member of Parliament</strong></a><br/>
-                   <a href="http://cherylgallant.com" className="text-white">https://johnyakabuski.com</a><br/>
-                   Cheryl Gallant<br/>
-                   cheryl.gallant@parl.gc.ca<br/>
-                </p>
-            </div>
-            <div className="rounded-lg w-full bg-green-500 p-5">
-                <p className="text-wrap">
-                <a href="https://www.arnprior.ca/en/town-hall/contact-us.aspx?_mid_=7524" className="text-[#165D8F]"><strong>Town of Arnprior</strong></a><br/>
-                   <a href="https://www.arnprior.ca/en/town-hall/contact-us.aspx?_mid_=7524" className="text-white">https://www.arnprior.ca/en/town-hall/contact-us.aspx?_mid_=7524</a><br/>
+        <div className="p-10 bg-green-200">
+            <h1 className="pb-0 mb-0 text-3xl font-bold text-center pb-10 text-black center">
+                <a href="https://www.arnprior.ca/en/town-hall/contact-us.aspx?_mid_=7524" className="text-[#165D8F]">
+                    <strong>Town of Arnprior</strong>
+                </a><br/>
+             </h1>
+            <p className="text-center pt-0 mt-0 underline">
+                <a href="https://www.arnprior.ca/en/town-hall/contact-us.aspx?_mid_=7524" className="text-green-800">
+                        https://www.arnprior.ca/en/town-hall/contact-us.aspx?_mid_=7524
+                </a>
+            </p>
+
+            <div className="mt-10 rounded-lg grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 md:gap-x-6 lg:gap-x-8 xl:grid-cols-3 h-full ">
+                <div className="rounded-lg w-full bg-white p-5">
                     Mayor: Lisa McGee<br/>
                     lmcgee@arnprior.ca<br/>
                     ​​613-322-0271<br/>
-                    <br/>
+                </div>
+                <div className="rounded-lg w-full bg-white p-5">
                     County Councillor: Dan Lynch<br/>
                     dlynch@arnprior.ca<br/>
                     613-623-0146<br/>
-                    <br/>
+                </div>
+                <div className="rounded-lg w-full bg-white p-5">
                     Councillor: Chris Toner<br/>
                     ctoner@arnprior.ca<br/>
                     613-913-9571<br/>
-                    <br/>
+                </div>
+                <div className="rounded-lg w-full bg-white p-5">
                     Councillor: Billy Denault<br/>
                     bdenault@arnprior.ca<br/>
                     613-447-7275<br/>
-                    <br/>
+                </div>
+                <div className="rounded-lg w-full bg-white p-5">
                     Councillor: Chris Couper<br/>
                     ccouper@arnprior.ca<br/>
                     613-518-4081<br/>
-                    <br/>
+                </div>
+                <div className="rounded-lg w-full bg-white p-5">
                     Councillor: Tom Burnette<br/>
                     tburnette@arnprior.ca<br/>
                     613 623 5630<br/>
-                    <br/>
+                </div>
+                <div className="rounded-lg w-full bg-white p-5">
                     Councillor: Lynn Cloutier<br/>
                     lcloutier@arnprior.ca<br/>
                     613-371-6036<br/>
-
-                </p>
+                </div>
             </div>
-
-
-
-        </div>
         </div>
         <p className="text-center mt-5 pb-10 text-2xl weight-bold pt-4 text-[#165D8F] ">Together we can find a better way!</p>
 
