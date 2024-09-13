9b9636c58dd0cf78e9c179eb6b9c7c24c393a147
Anne Brown <anne@browndomain.com>
2024-09-13 10:41:36 -0400

    Reinstated the 3 orig buttons

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -17,7 +17,7 @@ const Blog = () => {
 
 
           
-        <div className="w-full mx-auto">
+        <div className="w-full mx-auto my-20">
             <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 md:gap-x-6 lg:gap-x-8 xl:grid-cols-3 place-content-stretch">
 
 
