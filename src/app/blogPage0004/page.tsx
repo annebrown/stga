ecb9046d05a26570e936ff405f140d82bd78a674
Anne Brown <anne@browndomain.com>
2024-09-18 21:30:09 -0400

    link

--- src/app/blogPage0004/page.tsx
+++ src/app/blogPage0004/page.tsx
@@ -102,7 +102,7 @@ const BlogPage0004 = () => {
 Contact: Pierre Dufresne <br />
 pdufresne@thomascavanagh.ca <br />
 613 257-2918 <br />
-Find out about other ways that you can <Link href="/action">make a difference</Link>  <br /></p>
+Find out about other ways that you can <a href="/action" className="duration-300 hover:text-primary text-[#38AF34]">make a difference</a>  <br /></p>
 
                 </div>
             </div>
