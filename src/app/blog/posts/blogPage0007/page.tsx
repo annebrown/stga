c41fb4aa84af2bd8b8caa5ac19be0e6373fc6ac2
Anne Brown <anne@browndomain.com>
2024-10-17 09:29:27 -0400

    /class/className/  Next.js, not Nuxt.js

--- src/app/blog/posts/blogPage0007/page.tsx
+++ src/app/blog/posts/blogPage0007/page.tsx
@@ -43,7 +43,7 @@ const BlogPage0007 = () => {
                     <p 
                         className="mb-10 text-base font-medium leading-relaxed text-black sm:text-lg sm:leading-relaxed lg:text-base lg:leading-relaxed xl:text-lg xl:leading-relaxed text-justify"
                     >
-                       This is not a Done Deal- we don’t want a housing development beside and cutting into the Grove so what is our vision?
+                       This is not a Done Deal- we don&quot;t want a housing development beside and cutting into the Grove so what is our vision?
                     </p>
                     
                     <div className="aspect-[500/270] mb-8 self-center">
@@ -55,10 +55,10 @@ const BlogPage0007 = () => {
                         />
                     </div>
                
-                    <p class="mb-8 text-center italic">
+                    <p className="mb-8 text-center italic">
                         What perfect rule for happiness,—<br />
                         To live and help to live.<br />
-                        <span class="underline">The Pines by Ruby Archer</span> <br />
+                        <span className="underline">The Pines by Ruby Archer</span> <br />
                     </p>
 
                     <p className="mb-8">
@@ -68,7 +68,7 @@ const BlogPage0007 = () => {
                         This vision can become a reality. The deal has not been finalized and we know there is power in the collective voice and collective action.
                     </p>
                     <p className="mb-8">
-                        Right now, the most proactive thing we can all do is to continue to email Galilee, OMI Lacombe and Cavanagh. If you have already emailed them, please send a reminder letting them know that you still oppose this land being developed and have your voice heard. We support Community development, not housing development! More information, contact and templates can be found <a class="duration-300 hover:text-primary text-[#38AF34]" href="http://192.168.4.21:3003/action">here</a>.
+                        Right now, the most proactive thing we can all do is to continue to email Galilee, OMI Lacombe and Cavanagh. If you have already emailed them, please send a reminder letting them know that you still oppose this land being developed and have your voice heard. We support Community development, not housing development! More information, contact and templates can be found <a className="duration-300 hover:text-primary text-[#38AF34]" href="http://192.168.4.21:3003/action">here</a>.
                     </p>
                     <p className="mb-8">
                         In cooperation with the community, we still see the Nature Conservatory of Canada (NCC) as the most effective stewards of this land. Together we can build a future where the community gardens are expanded, the old-growth forest is left to grow and expand naturally and we honour the community use of this land as a place to connect and learn together.
@@ -80,7 +80,7 @@ const BlogPage0007 = () => {
                         We would like to take a moment to thank all the local businesses (listed below) for their support in sharing posters and selling buttons, the media coverage we’ve received (links below) and the community members who are writing letters, initiating conversations and showing their support with lawn signs. We couldn’t do it without YOU! Thank you
                     </p>
                     <p className="mb-8 text-center">
-                        Watch and Share our YouTube video <a href="https://www.youtube.com/embed/tEgtT1LX-Sk" class="duration-300 hover:text-primary text-[#38AF34]">What’s at Stake</a>
+                        Watch and Share our YouTube video <a href="https://www.youtube.com/embed/tEgtT1LX-Sk" className="duration-300 hover:text-primary text-[#38AF34]">What’s at Stake</a>
                     </p>
                     <p className="mb-8">
                         You can find the petition and buttons at the following local businesses:
@@ -103,9 +103,9 @@ const BlogPage0007 = () => {
                     <p className="mb-8 text-center">
                         Sweet & Sassy Cafe and Bistro<br />114 John St. N.
                     </p>
-                    <p class="bg-orange-400 p-2">Consider Moving to separate blog page because, literally burying the headline.  lol</p>
+                    <p className="bg-orange-400 p-2">Consider Moving to separate blog page because, literally burying the headline.  lol</p>
                     <p className="mb-8">Latest News and Stories:</p>
-                    <p class="bg-orange-400 p-2">Or continue here</p>
+                    <p className="bg-orange-400 p-2">Or continue here</p>
 
         
 
