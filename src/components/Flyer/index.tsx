b031012d17d640b61e2d2e59b24eb52cb2f023ff
Anne Brown <anne@browndomain.com>
2025-01-12 16:26:42 -0500

    New Version of Flyer

--- src/components/Flyer/index.tsx
+++ src/components/Flyer/index.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 
 const Flyer = () => {
   return (
-    <>
-      <section id="Flyer" className="realtive z-30 mt-0 p-0 w-full text-black">
+    <Flyer>
+      < id="Flyer" className="realtive z-30 mt-0 p-0 w-full text-black">
           
             <h2 className="text-3xl pb-3 font-bold text-center">January 16, 2025 Event Flyer</h2>
             <div className="text-center px-0">
@@ -13,7 +13,7 @@ const Flyer = () => {
             </div>
           <p className="text-center pb-10 text-2xl weight-bold pt-4 text-[#165D8F] ">Together we can find a better way!</p>
 
-      </section>
+      </Flyer>
     </>
   );
 };
