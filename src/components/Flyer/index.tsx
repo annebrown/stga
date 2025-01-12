7a02fc758bdf3fb7877ea03eb3bf4d88dabcc8d7
Anne Brown <anne@browndomain.com>
2025-01-12 16:31:35 -0500

    Incorrect component name

--- src/components/Flyer/index.tsx
+++ src/components/Flyer/index.tsx
@@ -4,8 +4,7 @@ import Image from 'next/image';
 
 const Flyer = () => {
   return (
-    <Flyer>
-      < id="Flyer" className="realtive z-30 mt-0 p-0 w-full text-black">
+    <section id="Flyer" className="realtive z-30 mt-0 p-0 w-full text-black">
           
             <h2 className="text-3xl pb-3 font-bold text-center">January 16, 2025 Event Flyer</h2>
             <div className="text-center px-0">
@@ -13,8 +12,8 @@ const Flyer = () => {
             </div>
           <p className="text-center pb-10 text-2xl weight-bold pt-4 text-[#165D8F] ">Together we can find a better way!</p>
 
-      </Flyer>
-    </>
+      </section>
+
   );
 };
 
