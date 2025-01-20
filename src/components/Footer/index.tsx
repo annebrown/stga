3a1d74d35a68de824caf94e10404de1653e2b499
Anne Brown <anne@browndomain.com>
2025-01-20 12:29:49 -0500

    Social Media Contact Icons Aligned

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -30,25 +30,27 @@ const Footer = () => {
 
 
 
-                    <div className="flex-none inline-block mx-auto text-black items-center m-0 mb-2 p-0 pb-1">
+                    <div className="flex-none inline-block mx-auto text-black items-center m-0 mb-1 p-0 pb-0">
                         <p className="text-center text-sm m-0 p-0"><strong>
                             Contacts
                         </strong></p>
 
-                        <p className="flex w-full mx-auto text-center mb-0 mt-4 pb-0 h-3">
+                        <p className="flex w-full mx-auto text-center mb-0 mt-2 pb-0 h-3">
                             <span className="flex mx-auto">
+
                                 <a href="mailto:savethegroveagain@gmail.com" 
                                     className="text-[#38AF34] hover:text-primary text-xs">
-
                                     <svg 
                                         xmlns="http://www.w3.org/2000/svg" 
-                                        width="18" height="18" viewBox="0 0 24 24">
+                                        width="18" height="18" 
+                                        className="inline-block align-bottom mt-1.5"
+                                        viewBox="0 0 24 24">
                                             <path 
                                                 fill="#38AF34" 
                                                 d="M5 5h13a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V8a3 3 0 0 1 3-3m0 1c-.5 0-.94.17-1.28.47l7.78 5.03l7.78-5.03C18.94 6.17 18.5 6 18 6zm6.5 6.71L3.13 7.28C3.05 7.5 3 7.75 3 8v9a2 2 0 0 0 2 2h13a2 2 0 0 0 2-2V8c0-.25-.05-.5-.13-.72z"/>
                                     </svg>
-                                </a>
-                                &nbsp;&nbsp;
+                                </a>&nbsp;&nbsp;
+                                
                                 <a
                                     href="https://www.youtube.com/embed/tEgtT1LX-Sk"
                                     aria-label="social-link"
@@ -60,7 +62,7 @@ const Footer = () => {
                                         width="18"
                                         height="14"
                                         viewBox="0 0 18 14"
-                                        className="fill-current inline-block mt-0 pt-1 align-top"
+                                        className="inline-block align-middle fill-current mt-0 pt-1"
                                         >
                                         <path d="M17.5058 2.07119C17.3068 1.2488 16.7099 0.609173 15.9423 0.395963C14.5778 7.26191e-08 9.0627 0 9.0627 0C9.0627 0 3.54766 7.26191e-08 2.18311 0.395963C1.41555 0.609173 0.818561 1.2488 0.619565 2.07119C0.25 3.56366 0.25 6.60953 0.25 6.60953C0.25 6.60953 0.25 9.68585 0.619565 11.1479C0.818561 11.9703 1.41555 12.6099 2.18311 12.8231C3.54766 13.2191 9.0627 13.2191 9.0627 13.2191C9.0627 13.2191 14.5778 13.2191 15.9423 12.8231C16.7099 12.6099 17.3068 11.9703 17.5058 11.1479C17.8754 9.68585 17.8754 6.60953 17.8754 6.60953C17.8754 6.60953 17.8754 3.56366 17.5058 2.07119ZM7.30016 9.44218V3.77687L11.8771 6.60953L7.30016 9.44218Z" />
                                     </svg>
@@ -71,7 +73,7 @@ const Footer = () => {
                                     aria-label="social-link"
                                     target="_blank"
                                     rel="noopener noreferrer"
-                                    className="duration-300 hover:text-primary text-[#38AF34] align-middle"
+                                    className="duration-300 hover:text-primary text-[#38AF34]"
                                 >
                                     <svg
                                     width="18"
@@ -79,6 +81,8 @@ const Footer = () => {
                                     viewBox="0 0 22 22"
                                     fill="none"
                                     xmlns="http://www.w3.org/2000/svg"
+                                    className="mt-1 inline-block align-middle fill-[#38AF34] stroke-[#38AF34]"
+
                                     >
                                     <path
                                         d="M12.1 10.4939V7.42705C12.1 6.23984 13.085 5.27741 14.3 5.27741H16.5V2.05296L13.5135 1.84452C10.9664 1.66676 8.8 3.63781 8.8 6.13287V10.4939H5.5V13.7183H8.8V20.1667H12.1V13.7183H15.4L16.5 10.4939H12.1Z"
@@ -94,9 +98,9 @@ const Footer = () => {
                                     <Image
                                         src="/images/brands/instagram.svg"
                                         alt="Instagram logo"
-                                        width="18"
-                                        height="18"
-                                        className="duration-300 hover:text-primary text-[#38AF34] fill-[#38AF34] stroke-[#38AF34] align-middle"
+                                        width="16"
+                                        height="16"
+                                        className="inline-block align-middle mt-1 duration-300 hover:text-primary text-[#38AF34] fill-[#38AF34] stroke-[#38AF34]"
                                          />
                                 </Link>
                             </span>
