f4b462145d7f4c73ff8dbe20d82e62d603d23c01
Anne Brown <anne@browndomain.com>
2025-01-22 09:09:42 -0500

    Img Optimization

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -21,7 +21,7 @@ const Hero = () => {
           /> */}
            <Image
             className="relative m-0 p-0 rounded-xl w-full"
-            src="/images/hero/big-postcard.svg"
+            src="/images/hero/big-postcard.png"
             alt="Postcard - Gilles Grove 10,000 years in the making"
             loading="eager"
             priority
@@ -32,7 +32,7 @@ const Hero = () => {
             <div className="mx-auto z-40 rounded-full">
               <Link href="/">
                 <Image
-                  src="/images/logo/logo_black_small.svg"
+                  src="/images/logo/logo-sq-detail.svg"
                   className="absolute top-10 right-10 w-1/5 h-auto"
                   alt="Logo - Trees and water inside a circle"
                   loading="eager"
