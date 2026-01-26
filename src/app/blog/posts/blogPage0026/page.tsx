09f1c63ed8e201c7f9223b0c5b249c3d2c119f8e
Anne Brown <anne@browndomain.com>
2026-01-26 16:19:40 -0500

    Production

--- src/app/blog/posts/blogPage0026/page.tsx
+++ src/app/blog/posts/blogPage0026/page.tsx
@@ -7,8 +7,8 @@ import Image from "next/image";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Coffeehouse: Katie Krelove",
-  description: "Special guest Katie Krelove from Ontario Wilderness Committee",
+  title: "First Coffeehouse of 2026!",
+  description: "with special guest Katie Krelove from Ontario Wilderness Committee",
   // other metadata
 };
 
